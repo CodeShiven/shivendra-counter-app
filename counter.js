9802d88adbb0ca1f59c9f13ebfbe3576561dc6d6
Shivendra vishwakarma <vshivendra1999@gmail.com>
2026-05-10 16:46:27 +0530

    Completed counter app functionality and styling

--- counter.js
+++ counter.js
@@ -1,1 +1,18 @@
-let count = 0;
+let count = 0
+
+const counter = document.getElementById("counter")
+
+function increase() {
+    count++
+    counter.innerHTML = count
+}
+
+function decrease() {
+    count--
+    counter.innerHTML = count
+}
+
+function reset() {
+    count = 0
+    counter.innerHTML = count
+}
